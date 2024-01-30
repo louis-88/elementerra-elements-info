73b2ff9b515807b86ba92f09b74e8b6a78662345
louis88 <35930080+louis-88@users.noreply.github.com>
2024-01-30 22:25:18 +0100

    Update elementerra-elements-info.js
    telescope found

--- elementerra-elements-info.js
+++ elementerra-elements-info.js
@@ -76,7 +76,7 @@ const elements = {
     "Nail": { "tier": "3", "amount": "340,000" },
     "Gas": { "tier": "3", "amount": "10,000" },
     "Bottle": { "tier": "3", "amount": "10,000" },
-    "Telescope": { "tier": "3", "amount": "10,000" },
+    "Telescope": { "tier": "3", "amount": "460,000" },
     "Storm": { "tier": "3", "amount": "10,000" },
     "Apple": { "tier": "4", "amount": "10,000" },
     "Music": { "tier": "4", "amount": "10,000" },
